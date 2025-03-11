076ba57e8f5131b8e833e6d424d99b18b9733236
Pedro Santos <p3dro.santosc@gmail.com>
2025-03-11 04:52:36 -0300

    feat: ensure password encryption in the database with bcrypt

--- src/use-cases/users/create-user.ts
+++ src/use-cases/users/create-user.ts
@@ -7,6 +7,8 @@ import { EmailAlReadyExistsError } from "../../errors/user";
 import { User } from "../../models/user";
 import { GetUserByEmailRepository } from "../../repositories/user/get-user-by-email";
 
+const bcrypt = require("bcrypt");
+
 export class CreateUserUseCase implements ICreateUserUseCase {
   constructor(
     private readonly createUserRepository: ICreateUserRepository,
@@ -21,6 +23,13 @@ export class CreateUserUseCase implements ICreateUserUseCase {
       throw new EmailAlReadyExistsError();
     }
 
-    return this.createUserRepository.createUser(params);
+    const hashedPassword = await bcrypt.hash(params.password, 10);
+
+    const user = {
+      ...params,
+      password: hashedPassword,
+    };
+
+    return this.createUserRepository.createUser(user);
   }
 }
