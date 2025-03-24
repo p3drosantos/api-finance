fead1cc8504ffb601264b4d05dac4d2f6ba4fcf3
Pedro Santos <p3dro.santosc@gmail.com>
2025-03-24 20:50:50 -0300

    fix: ensure import correctly

--- src/use-cases/transaction/create-transaction.ts
+++ src/use-cases/transaction/create-transaction.ts
@@ -5,25 +5,26 @@ import {
   ICreateTransactionRepository,
   ICreateTransactionUseCase,
 } from "../../controllers/transaction/create-transaction/protocols";
-import { GetUserByEmailRepository } from "../../repositories/user/get-user-by-email";
 import { UserNotFoundError } from "../../errors/user";
+import { GetUserByIdRepository } from "../../repositories/user/get-user-by-id";
 
 export class CreateTransactionUseCase implements ICreateTransactionUseCase {
   constructor(
     private readonly transactionRepository: ICreateTransactionRepository,
-    private readonly getUserByIdRepository: GetUserByEmailRepository,
+    private readonly getUserByIdRepository: GetUserByIdRepository,
   ) {}
 
   async create(params: CreateTransactionParams): Promise<PrismaTransaction> {
     const userId = params.userId;
 
-    const user = await this.getUserByIdRepository.getByEmail(userId);
+    const user = await this.getUserByIdRepository.getUserById(userId);
 
     if (!user) {
       throw new UserNotFoundError();
     }
 
-    const transaction = await this.transactionRepository.create(params);
+    const transaction =
+      await this.transactionRepository.createTransaction(params);
 
     return transaction;
   }
