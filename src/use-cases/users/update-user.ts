69238880045204d965e912cac6e7b0d95b105757
Pedro Santos <p3dro.santosc@gmail.com>
2025-03-29 23:18:19 -0300

    refactor: ensure password is hashed on update user

--- src/use-cases/users/update-user.ts
+++ src/use-cases/users/update-user.ts
@@ -4,12 +4,20 @@ import {
   UpdateUserParams,
 } from "../../controllers/user/update-user/protocols";
 import { User } from "../../models/user";
+const bcrypt = require("bcrypt");
 
 export class UpdateUserUseCase implements IUpdateUserUseCase {
   constructor(private readonly updateUserRepository: IUpdateUserRepository) {}
 
   async execute(id: string, params: UpdateUserParams): Promise<User> {
-    const user = await this.updateUserRepository.updateUser(id, params);
-    return user;
+    const user = { ...params };
+
+    if (params.password) {
+      const hashedPassword = await bcrypt.hash(params.password, 10);
+      user.password = hashedPassword;
+    }
+
+    const updateUser = await this.updateUserRepository.updateUser(id, user);
+    return updateUser;
   }
 }
